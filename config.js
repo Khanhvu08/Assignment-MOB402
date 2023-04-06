3fda1f7bd3af562062e336612b050c61a3d614dc
Sat thu da tinh <94190976+Khanhvu08@users.noreply.github.com>
2023-04-06 11:37:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    uri: 'mongodb+srv://khanhvvph28077:wwwxnhv3FvK2vJ6M@cluster0.4rvt0wr.mongodb.net/ASM-MOB402?retryWrites=true&w=majority',
+    uri: '',
     sessionSecretKey: 'Anuisdfgb8192BSIufkboa'
 };
